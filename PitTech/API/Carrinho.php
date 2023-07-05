8c6a7fd93271bb33d729d76eb1d27f254004ed89
Bruno La Gatta <brunolagatta55525@gmail.com>
2023-07-05 16:53:28 -0300

    comit

--- PitTech/API/Carrinho.php
+++ PitTech/API/Carrinho.php
@@ -8,11 +8,24 @@ class Carrinho{
     }
 
     public function adicionar($id_usr, $id_prod) {
-        
+        $sql = "INSERT INTO carrinho (Usuario_idUsuario, produto_idproduto, comprado) VALUES ('$id_usr','$id_prod','0')";
+        $stmt = $this->conn->prepare($sql);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function remover($id_usr, $id_prod){
-
+        $sql = "DELETE FROM carrinho WHERE Usuario_idUsuario='$id_usr' and produto_idproduto='$id_prod'";
+        $stmt = $this->conn->prepare($sql);
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
